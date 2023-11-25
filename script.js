9b8f096fa8577a7a828714f0d515596625d68eb8
MurjaAmadiKurfi <147001145+MurjaAmadiKurfi@users.noreply.github.com>
2023-11-25 13:35:41 +0100

    Add files via upload
    On Click background color change

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
 // script.js
 
-document.addEventListener("DOMContentLoaded", function() {
-    // This function will be called when the DOM is fully loaded
+function changeBackgroundColor() {
+    // Get the body element
+    var body = document.body;
 
-    // Prompt the user for their name
-    var userName = prompt("Please enter your name:");
-
-    // Check if the user entered a name
-    if (userName !== null && userName !== "") {
-        // Use document.write() to display "Hello username" in big bold letters
-        document.write("<h1>Hello <strong>" + userName + "</strong>!</h1>");
-
-        // Display an alert message
-        alert("Hello " + userName + "!");
-    } else {
-        // If the user didn't enter a name, provide a default welcome message
-        alert("You didn't enter a name. Welcome anyway!");
-    }
-});
+    // Change the background color to blue
+    body.style.backgroundColor = "blue";
+}
